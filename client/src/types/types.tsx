b09c1bb123921bcc38c888e72fa1e02453bd81c2
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-01-01 03:42:34 +0800

    UserType added ( types )

---
+++ client/src/types/types.tsx
@@ -0,0 +1,11 @@
+export type UserType = {
+    id?: number;
+    username: string;
+    firstname: string;
+    lastname: string;
+    email?: string;
+    number: number;
+    address?: string;
+    password: string;
+    confirm_password?: string;
+}
