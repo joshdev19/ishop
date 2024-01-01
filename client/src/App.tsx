a978ae9273d6bc70358854b65f8ade31e261e199
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-01-01 14:48:14 +0800

    Routes for Login & Signup is set

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 const App = () => {
 
   return (
     <Routes>
-      <Route />
+      <Route path="/signup" element={ <Signup/> } />
+      <Route path="/login" element={ <Login/> } />
     </Routes>
   )
 
