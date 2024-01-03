096c807f5eb76ad33658ce1526e2d3c68e810621
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-01-03 14:44:55 +0800

    Route for Products added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Products from "./pages/Products";
 
 const App = () => {
 
@@ -8,6 +9,7 @@ const App = () => {
     <Routes>
       <Route path="/signup" element={ <Signup/> } />
       <Route path="/login" element={ <Login/> } />
+      <Route path="/" element={ <Products/> } />
     </Routes>
   )
 
