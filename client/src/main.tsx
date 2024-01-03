b3029da61e4fe657698a09fba14c0901ef4b3cb0
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-01-03 16:06:27 +0800

    New package installed

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios'
+
+axios.defaults.baseURL = "http://localhost:5000/api";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
